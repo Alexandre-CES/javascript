6da8eafc80e7a007406c3bc4b5e881b21d65925f
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2023-10-29 22:46:41 -0300

    Introdução à condições

---
+++ exercicios/modulo-d/ex008.js
@@ -0,0 +1,7 @@
+var sas = 49.9
+
+if (sas >= 50){
+    console.log(`você tem mais de 50 veio broxa. Pra ser exato, ${sas} veio broxa`)
+} else{
+    console.log(`Você tem menos de 50 veio broxa. Pra ser exato, ${sas} veio broxa`)
+}
---
+++ exercicios/modulo-d/ex009.js
@@ -0,0 +1,8 @@
+var carlinhos = 'Chupador de cavalos'
+
+if (carlinhos == 'Chupador de cavalos'){
+    console.log(`Carlinhos é o maior ${carlinhos} do mundo, ele bateu o recorde de chupar semen de cavalos`)
+
+}else{
+    console.log('Você não é carlinhos')
+}
